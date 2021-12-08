61a75e236d037a4b2a1ed7d89a0143d03411b689
RodikYermak <rodion.yermakov.sd@gmail.com>
2021-12-08 08:52:01 -0800

    refactored props for Occupation overview component

--- src/App.js
+++ src/App.js
@@ -7,22 +7,14 @@ import data from './data'
 
 function App() {
   console.log(data)
-  const occupationTitle = data.occupation.title
-  const regionTitle = data.region.title
-  const summaryJobsRegional = data.summary.jobs.regional
-  const summaryJobsYear = data.summary.jobs.year
-  const summaryJobsNationalAvg = data.summary.jobs.national_avg
-  const summaryJobsNationalAvgPercentage = Math.round(summaryJobsRegional/summaryJobsNationalAvg*100)
+
+  const occupationData = data.occupation
+  const regionData = data.region
+  const summaryData = data.summary
 
 
-  const summaryJobsGrowthRegional = data.summary.jobs_growth.regional
-  const summaryJobsGrowthStartYear = data.summary.jobs_growth.start_year
-  const summaryJobsGrowthEndYear = data.summary.jobs_growth.end_year
-  const summaryJobsGrowthNationalAvg = data.summary.jobs_growth.national_avg
 
-  const summaryEarningsRegional = data.summary.earnings.regional
-  const summaryEarningsNationalAvg = data.summary.earnings.national_avg
-
+  
 
   const trendComparisonStartYear = data.trend_comparison.start_year
   const trendComparisonEndYear = data.trend_comparison.end_year
@@ -37,24 +29,11 @@ function App() {
   // console.log(trendComparisonRegional)
   return (
     <div className="app-container">
-      <OccupationOverview 
-        occupationTitle={occupationTitle} 
-        regionTitle={regionTitle}
-        summaryJobsRegional={summaryJobsRegional} 
-        summaryJobsYear={summaryJobsYear} 
-        summaryJobsNationalAvg={summaryJobsNationalAvg}
-        summaryJobsGrowthRegional={summaryJobsGrowthRegional}
-        summaryJobsGrowthStartYear={summaryJobsGrowthStartYear}
-        summaryJobsGrowthEndYear={summaryJobsGrowthEndYear}
-        summaryJobsGrowthNationalAvg={summaryJobsGrowthNationalAvg}
-        summaryEarningsRegional={summaryEarningsRegional}
-        summaryEarningsNationalAvg={summaryEarningsNationalAvg}
-        summaryJobsNationalAvgPercentage={summaryJobsNationalAvgPercentage}
-      />
+      <OccupationOverview occupation={occupationData} region={regionData} summary={summaryData} />
       
-      <RegionalTable startYear = {trendComparisonStartYear} endYear={trendComparisonEndYear} region={trendComparisonRegional} state={trendComparisonState} nation={trendComparisonNation}/>
+      {/* <RegionalTable startYear = {trendComparisonStartYear} endYear={trendComparisonEndYear} region={trendComparisonRegional} state={trendComparisonState} nation={trendComparisonNation}/> */}
       
-      <IndustriesEmploying title={occupationTitle} industries={employingIndustries}/>
+      {/* <IndustriesEmploying title={occupationTitle} industries={employingIndustries}/> */}
      
     </div>
   );
--- src/components/OccupationOverview.js
+++ src/components/OccupationOverview.js
@@ -1,26 +1,44 @@
 import React from 'react'
 
 function OccupationOverview(props) {
+    // occupation
+    let occupationTitle = props.occupation.title
+    // region
+    let regionTitle = props.region.title
+    // summary -> jobs
+    let jobsRegional = props.summary.jobs.regional
+    let jobsYear = props.summary.jobs.year
+    let jobsNationalAvg = props.summary.jobs.national_avg
+    let jobsNationalAvgPercentage = Math.round(jobsRegional/jobsNationalAvg*100)
+    // summary -> jobs_growth
+    let jobsGrowthRegional = props.summary.jobs_growth.regional
+    let jobsGrowthStartYear = props.summary.jobs_growth.start_year
+    let jobsGrowthEndYear = props.summary.jobs_growth.end_year
+    let jobsGrowthNationalAvg = props.summary.jobs_growth.national_avg
+    // summary -> earnings
+    let earningsRegional = props.summary.earnings.regional
+    let earningsNationalAvg = props.summary.earnings.national_avg
+  
     return (
         <div>
             <h1>Occupation Overview</h1>
-            <p className="occupation-title">{props.occupationTitle} in {props.regionTitle}</p>
-            <h2>Occupation Summary for {props.occupationTitle}</h2>
+            <p className="occupation-title">{occupationTitle} in {regionTitle}</p>
+            <h2>Occupation Summary for {occupationTitle}</h2>
             <div className="occupation-cards-container">
                 <div className="occupation-card">
-                    <p className="occupation-card-top">{props.summaryJobsRegional.toLocaleString('en-US')}</p>
-                    <p className="occupation-card-middle">Jobs ({props.summaryJobsYear})</p>
-                    <p className="occupation-card-bottom">{props.summaryJobsNationalAvgPercentage}% <span className="green">above</span> National average</p>
+                    <p className="occupation-card-top">{jobsRegional.toLocaleString('en-US')}</p>
+                    <p className="occupation-card-middle">Jobs ({jobsYear})</p>  
+                    {jobsNationalAvgPercentage>100 ? <p className="occupation-card-bottom">{jobsNationalAvgPercentage}% <span className="green">above</span> National average</p> : <p className="occupation-card-bottom">{jobsNationalAvgPercentage}% <span className="red">below</span> National average</p>}
                 </div>    
                 <div className="occupation-card">
-                    <p className="occupation-card-top green">+{props.summaryJobsGrowthRegional}%</p>
-                    <p className="occupation-card-middle">% Change ({props.summaryJobsGrowthStartYear}-{props.summaryJobsGrowthEndYear})</p>
-                    <p className="occupation-card-bottom">Nation: <span className="green">+{props.summaryJobsGrowthNationalAvg}%</span></p>
+                    {jobsGrowthRegional > 0 ? <p className="occupation-card-top green">+{jobsGrowthRegional}%</p> : <p className="occupation-card-top red">{jobsGrowthRegional}%</p>}
+                    <p className="occupation-card-middle">% Change ({jobsGrowthStartYear}-{jobsGrowthEndYear})</p>
+                    {jobsGrowthNationalAvg > 0 ? <p className="occupation-card-bottom">Nation: <span className="green">+{jobsGrowthNationalAvg}%</span></p> : <p className="occupation-card-bottom">Nation: <span className="red">{jobsGrowthNationalAvg}%</span></p>}
                 </div>  
                 <div className="occupation-card no-right-border">
-                    <p className="occupation-card-top">${props.summaryEarningsRegional}/hr</p>
+                    <p className="occupation-card-top">${earningsRegional}/hr</p>
                     <p className="occupation-card-middle">Median Hourly Earnings</p>
-                    <p className="occupation-card-bottom">Nation: ${props.summaryEarningsNationalAvg}/hr</p>
+                    <p className="occupation-card-bottom">Nation: ${earningsNationalAvg}/hr</p>
                 </div>  
             </div>
         </div>
