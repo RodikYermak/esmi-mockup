3ac9f6bb792b6d18a534269818ce9a8df785c6a7
RodikYermak <rodion.yermakov.sd@gmail.com>
2021-12-09 22:17:28 -0800

    update btn

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import axios from 'axios'
 import IndustriesEmploying from './components/IndustriesEmploying';
 import OccupationOverview from './components/OccupationOverview';
 import RegionalTable from './components/RegionalTrends';
+
 // import data from './data'
 import jsonData from './ProjectSampleResponseData'
 
@@ -9,38 +11,33 @@ function App() {
   const [error, setError] = React.useState(null);
   const [isLoaded, setIsLoaded] = React.useState(false);
   const [emsiData, setEmsiData] = React.useState()
+  
   const url = "https://run.mocky.io/v3/a2cc3707-8691-4188-8413-6183a7bb3d32"
+    
   React.useEffect(()=>{
-    fetch(url)
-      .then(res=>res.json())
-      .then(result=>{
+    axios
+      .get(url)
+      .then(response => {
+        console.log(response.data)
+        setEmsiData(response.data);
         setIsLoaded(true);
-        setEmsiData(result); 
-      },
-      (error) => {
-        setIsLoaded(true);
+      })
+      .catch(error => {
         setError(error);
-      }
-    )
+      })
   },[])
   
   const handleUpdating = () => {
-    console.log(url, 'updating', jsonData)
     setEmsiData(jsonData)
+    // axios
+    //   .post(url, jsonData)
+    //   .then(response => {
+    //     console.log(response)
+    //   })
+    //   .catch(error => {
+    //     setError(error);
+    //   })
   }
-
-  // posting data 
-  React.useEffect(() => {
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(jsonData)
-    };
-    fetch(url, requestOptions)
-        .then(response => response.json())
-        .then(data => setEmsiData(data));
-}, []);
-
 
   if (error) {
     return <div>Error: {error.message}</div>;
@@ -49,7 +46,7 @@ function App() {
   } else {
     return (
     <div className="app-container">
-      <button onClick={handleUpdating}>Get Data for Graphic Designers</button>
+      <button className="btn" onClick={handleUpdating}>Update to ProjectSampleResponseData</button>
       {emsiData && <OccupationOverview 
         occupation={emsiData.occupation} 
         region={emsiData.region} 
