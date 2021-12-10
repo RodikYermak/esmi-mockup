b47c225bb06d50621322425478f72ec5b2770b93
RodikYermak <rodion.yermakov.sd@gmail.com>
2021-12-10 11:35:25 -0800

    final update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import axios from 'axios'
 import IndustriesEmploying from './components/IndustriesEmploying';
 import OccupationOverview from './components/OccupationOverview';
-import RegionalTable from './components/RegionalTrends';
+import RegionalTrends from './components/RegionalTrends';
 
 // import data from './data'
 import jsonData from './ProjectSampleResponseData'
@@ -10,6 +10,7 @@ import jsonData from './ProjectSampleResponseData'
 function App() {
   const [error, setError] = React.useState(null);
   const [isLoaded, setIsLoaded] = React.useState(false);
+  const [isDisplayed, setIsDisplayed] = React.useState(true)
   const [emsiData, setEmsiData] = React.useState()
   
   const url = "https://run.mocky.io/v3/a2cc3707-8691-4188-8413-6183a7bb3d32"
@@ -18,7 +19,6 @@ function App() {
     axios
       .get(url)
       .then(response => {
-        console.log(response.data)
         setEmsiData(response.data);
         setIsLoaded(true);
       })
@@ -29,6 +29,9 @@ function App() {
   
   const handleUpdating = () => {
     setEmsiData(jsonData)
+    setIsDisplayed(false)
+    // POST METHOD IS NOT WORKING FOR SOME REASONS
+    // MAYBE THERE ARE SOME RESTRICTIONS ON UPDATING DATA 
     // axios
     //   .post(url, jsonData)
     //   .then(response => {
@@ -46,13 +49,13 @@ function App() {
   } else {
     return (
     <div className="app-container">
-      <button className="btn" onClick={handleUpdating}>Update to ProjectSampleResponseData</button>
+      {isDisplayed && <button className="btn" onClick={handleUpdating}>Display {jsonData.occupation.title} overview</button>}
       {emsiData && <OccupationOverview 
         occupation={emsiData.occupation} 
         region={emsiData.region} 
         summary={emsiData.summary} 
       />}
-      {emsiData && <RegionalTable 
+      {emsiData && <RegionalTrends 
         trend={emsiData.trend_comparison} 
       />}
       {emsiData && <IndustriesEmploying 
